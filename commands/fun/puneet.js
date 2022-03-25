52c1aca91f85b39d944702be792bd49ff1cd5537
universe-echo <ushabhati0202@gmail.com>
2022-03-25 19:53:42 +0530

    lol

---
+++ commands/fun/puneet.js
@@ -0,0 +1,18 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "puneet",
+  aliases: [''],
+  cooldown: 10,
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    
+
+message.reply("`Relationships = 3`")
+
+    },
+};
