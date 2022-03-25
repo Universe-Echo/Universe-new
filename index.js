91b03c50676f5207455577595a88d86584c01fa2
universe-echo <ushabhati0202@gmail.com>
2022-03-25 22:33:21 +0530

    aaaaa

--- index.js
+++ index.js
@@ -15,40 +15,7 @@ require("./handler")(client);
 
 //error handler 
 process.on("unhandledRejection", console.error);
-const errchannel = '952060219830206534'
- process.on('unhandledRejection', (reason, p) => {
-    console.log(' [Anti-Crash] ')
-    console.log(reason, p)
-    const errEmbed = new MessageEmbed()
-    .setColor('DARK_BUT_NOT_BLACK')
-    .setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-    .setTitle('Error (unhandledRejection)')
-    .setDescription(` \`\`\`\js\n${reason}\n\n${p} \`\`\`\ `)
-    .setTimestamp()
-    client.channels.cache.get(errchannel).send({embeds: [errEmbed]})
-})
-process.on('uncaughtException', (err, origin) => {
-    console.log(' [Anti-Crash] ')
-    console.log(err, origin)
-    const errEmbed = new MessageEmbed()
-    .setColor('DARK_BUT_NOT_BLACK')
-    .setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-    .setTitle('Error (uncaughtException)')
-    .setDescription(` \`\`\`\js\n${err}\n\n${origin} \`\`\`\ `)
-    .setTimestamp()
-    client.channels.cache.get(errchannel).send({embeds: [errEmbed]})
-})
-process.on('multipleResolves', (type, promise, reason) => {
-  console.log(' [Anti-Crash] ')
-  console.log(type, promise, reason)
-  const errEmbed = new MessageEmbed()
-  .setColor('DARK_BUT_NOT_BLACK')
-  .setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-  .setTitle('Error (multipleResolves)')
-  .setDescription(` \`\`\`\js\n${type}\n\n${promise}\n\n${reason} \`\`\`\ `)
-  .setTimestamp()
-  client.channels.cache.get(errchannel).send({embeds: [errEmbed]})
-})
-
+process.on('uncaughtException', console.error)
+process.on('multipleResolves', console.error)
 
 client.login(process.env.TOKEN);
