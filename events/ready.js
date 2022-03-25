ecb5f716fc86a6b0c4d304f47a340b1ca9a66837
universe-echo <ushabhati0202@gmail.com>
2022-03-25 18:53:50 +0530

    aaaaa

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 client.on("ready", () =>
     //console.log(`${client.user.tag} is up and ready to go!`)
    
-    client.user.setActivity("Under testing", {
+    client.user.setActivity("Under maintenance", {
       type: "STREAMING",
       url: "https://www.youtube.com/watch?v=qj9aJL_lXN8"
     })
