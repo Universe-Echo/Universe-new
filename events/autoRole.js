175e692d37bcbc71375708be33828f7fb4941efe
universe-echo <ushabhati0202@gmail.com>
2022-03-26 15:30:21 +0530

    aa

--- events/autoRole.js
+++ events/autoRole.js
@@ -2,11 +2,13 @@ const db2 = require('../models/autorole')
 
 const client = require('../index')
 client.on('guildMemberAdd', async (member) => {
+  if(member.user.bot) return;
   db2.findOne({ Guild: member.guild.id}, async(e, data) => {
     if(!data) return
 
     const role = member.guild.roles.cache.get(data.ROLE)
     member.roles.add(role)
   //  await db2.findOne(`autorole-${member.guild.id}`)
+ 
 })
 })
