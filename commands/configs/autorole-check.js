26c9165da7e5a8732423bf991510ec0e7e7e0957
universe-echo <ushabhati0202@gmail.com>
2022-04-26 09:44:11 +0530

    test

--- commands/configs/autorole-check.js
+++ commands/configs/autorole-check.js
@@ -4,10 +4,10 @@ module.exports = {
     name: 'autorole-check',
     cooldown: 5,
     description: 'check autorole of the server',
-userPermissions: ['MANAGE_ROLES'],
-botPermissions: ['ADMINISTRATOR'],
-usage: 'autorole-check',
-   run: async (client, message, args) => {
+    userPermissions: ['MANAGE_ROLES'],
+    botPermissions: ['ADMINISTRATOR'],
+    usage: 'autorole-check',
+    run: async (client, message, args) => {
         if (!message.member.guild.me.permissions.has('ADMINISTRATOR')) {
             return message.channel.send('**I dont have the perms to do this!**')
         }
@@ -16,18 +16,18 @@ usage: 'autorole-check',
             return message.channel.send('**You dont have the perms to do this!**')
 
         }
-        Schema.findOne({ Guild: message.guild.id}, async(err, data) => {
-     
-            if(!data) return message.reply('**Autorole is not set yet**!')
-            
-           const role = (data.ROLE)
+        Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
+
+            if (!data) return message.reply('**Autorole is not set yet**!')
+
+            const role = (data.ROLE)
             const roleEmbed = new MessageEmbed()
-            .setDescription(`**Autorole => <@&${role}>**`)
-            message.reply({embeds: [roleEmbed]})
-            
-            
-                    })
-     
+                .setDescription(`**Autorole => <@&${role}>**`)
+            message.reply({ embeds: [roleEmbed] })
+
+
+        })
+
 
 
     }
