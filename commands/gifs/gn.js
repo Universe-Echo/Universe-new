f5f705e3409c00d00abdb66fd3e19b916a256c3b
universe-echo <ushabhati0202@gmail.com>
2022-05-05 10:47:33 +0530

    a

--- commands/gifs/gn.js
+++ commands/gifs/gn.js
@@ -6,6 +6,6 @@ module.exports = {
     description: 'gn gif',
     usage: 'gn',
     run: async (client, message, args) => {
-        message.channel.send('https://tenor.com/view/sleep-good-night-cat-gif-24504919')
+        message.channel.send('jaldi sona chahiye')
     }
 }
