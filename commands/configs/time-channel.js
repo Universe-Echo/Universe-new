a01980493ff9a6e1bf68c27461f59179cdc9e20e
universe-echo <ushabhati0202@gmail.com>
2022-05-15 12:16:52 +0530

    ds

--- commands/configs/time-channel.js
+++ commands/configs/time-channel.js
@@ -16,16 +16,16 @@ module.exports = {
      */
     run: async (client, message, args) => {
         let TIMEZONE = "Asia/Calcutta"; // Example: EST - Read more: [ https://en.wikipedia.org/wiki/List_of_tz_database_time_zones# ]
-        let FORMAT = "LT"; // Example: EST - Read more: [ https://momentjs.com/docs/#/displaying/format/ ]
+        let FORMAT = "H"; // Example: EST - Read more: [ https://momentjs.com/docs/#/displaying/format/ ]
         let CHANNEL_ID = "975285698854809630"; // Example: 823605956348608512
         let UPDATE_INTERVAL = "600000"; // is in ms!!!
 
         const timeNow = moment().tz(TIMEZONE);
         const timeChannel = client.channels.cache.get(CHANNEL_ID);
-        timeChannel.edit({ name: `⏰ ${timeNow}` }, "Updated")
+        timeChannel.edit({ name: `${timeNow}` }, "Updated")
         setInterval(() => {
             const updatedTime = moment().tz(TIMEZONE).format(FORMAT);
-            timeChannel.edit({ name: `⏰ ${updatedTime}` }, "Updated");
+            timeChannel.edit({ name: `${updatedTime}` }, "Updated");
         }, UPDATE_INTERVAL);
         setTimeout(() => {
             console.log("Stopping process with the code \"0\"...");
