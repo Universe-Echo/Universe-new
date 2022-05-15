a9bd1903cf934b31c9f1aff22eb469b57d8c7cdf
universe-echo <ushabhati0202@gmail.com>
2022-05-15 11:36:17 +0530

    dfds

--- events/ready.js
+++ events/ready.js
@@ -12,31 +12,17 @@ client.on("ready", () => {
   const servers = client.guilds.cache.size;
   const users = client.users.cache.size;
 
-  let totalSeconds = (client.uptime / 1000);
-  let days = Math.floor(totalSeconds / 86400);
-  totalSeconds %= 86400;
-  let hours = Math.floor(totalSeconds / 3600);
-  totalSeconds %= 3600;
-  let minutes = Math.floor(totalSeconds / 60);
-  let seconds = Math.floor(totalSeconds % 60);
   
-  let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
 
-  let statuses = [`${servers} servers | >help`, `${users} users | >help`, `Up since: ${minutes}mins`]
-
-  let index = 0
-  setInterval(() => {
-    const randomIndex = Math.floor(Math.random() * (statuses.length));
-    const newActivity = statuses[randomIndex];
+  
     client.user.setPresence({
       activities: [{
-        name: `${newActivity}`,
+        name: `>help`,
         type: "WATCHING",
         // url: `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
       }],
       status: "online",
     })
-  }, 5000)
 
 
   // client.user.setActivity("Under maintenance", {
