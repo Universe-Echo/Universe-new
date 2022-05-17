2a8abbd58d26da0060c488b10b2ccf592e18d05c
universe-echo <ushabhati0202@gmail.com>
2022-05-17 23:13:54 +0530

    ds

--- events/modal.js
+++ events/modal.js
@@ -1,6 +1,7 @@
 const client = require('../index');
 
 client.on('interactionCreate', interaction => {
+    await interaction.deferReply({ ephemeral: false }).catch(() => {});
 	if (!interaction.isModalSubmit()) return;
     interaction.deferReply({ ephemeral: true });
 	const short = interaction.fields.getTextInputValue('short');
