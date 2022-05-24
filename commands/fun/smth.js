1edd4160b32870ba9b7d9debe0d905b640a9ec08
universe-echo <ushabhati0202@gmail.com>
2022-05-24 18:00:49 +0530

    zeeeeee

---
+++ commands/fun/smth.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'anna',
+    aliases: ['dumbo'],
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {String[]} args 
+ */
+
+    run: async (client, message, args) => {
+        message.channel.send('is ananya')
+
+
+
+
+    }
+}
