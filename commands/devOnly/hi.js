3b37eff862c0dee656251e02be830a44346ae2d2
universe-echo <ushabhati0202@gmail.com>
2022-05-25 23:22:37 +0530

    hello

--- commands/devOnly/hi.js
+++ commands/devOnly/hi.js
@@ -15,12 +15,10 @@ var superagent = require('superagent');
 */
 module.exports = {
   name: "hi1",
-  category: "🔞 NSFW",
-  usage: "porn",
-  type: "real",
+ devOnly: true,
   run: async (client, message, args) => {
 
-  
+  if(!message.author.id === '673846605920600068') return;
 
     superagent.get('https://nekobot.xyz/api/image').query({
       type: 'pgif'
